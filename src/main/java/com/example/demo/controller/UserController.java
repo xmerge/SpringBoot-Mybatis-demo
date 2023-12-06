5c362db457996a631a424af52cb005f2b9a6a4dd
xmerge <zxmerge@gmail.com>
2023-12-06 22:55:05 +0800

    初始化MyBatis(XML)

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,85 +1,36 @@
 package com.example.demo.controller;
 
-import com.example.demo.entity.User;
+import com.example.demo.entity.UserDO;
+import com.example.demo.service.UserService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
-import java.util.*;
+import javax.annotation.Resource;
 
 @RestController
 @RequestMapping(value = "/users")     // 通过这里配置使下面的映射都在/users下
 @Api(tags = "用户管理",value = "User Controller")
 public class UserController {
-
-    // 创建线程安全的Map，模拟users信息的存储
-    static Map<Long, User> users = Collections.synchronizedMap(new HashMap<>());
-
-    /**
-     * 处理"/users/"的GET请求，用来获取用户列表
-     *
-     * @return 用户列表
-     */
-    @GetMapping("/")
-    @ApiOperation("获取用户列表")
-    public List<User> getUserList() {
-        // 还可以通过@RequestParam从页面中传递参数来进行查询条件或者翻页信息的传递
-        return new ArrayList<>(users.values());
+    @Resource
+    private UserService userService;
+    @PostMapping(value = "/insert")
+    @ApiOperation(value = "插入用户", notes = "向数据库中插入用户信息")
+    public int insert(@RequestBody UserDO user) {
+        return userService.insert(user);
     }
 
-    /**
-     * 处理"/users/"的POST请求，用来创建User
-     *
-     * @param user 用户信息
-     * @return 结果
-     */
-    @PostMapping("/")
-    @ApiOperation(value = "创建用户", notes = "创建用户")
-    public String postUser(@Valid @RequestBody User user) {
-        // @RequestBody注解用来绑定通过http请求中application/json类型上传的数据
-        users.put(user.getId(), user);
-        return "success";
+    @PostMapping(value = "/deleteUserByName")
+    @ApiOperation(value = "根据用户名删除用户", notes = "根据用户名从数据库中删除用户信息")
+    public int deleteUserByName(@RequestBody String username) {
+        return userService.deleteByUserName(username);
     }
-
-    /**
-     * 处理"/users/{id}"的GET请求，用来获取url中id值的User信息
-     *
-     * @param id id
-     * @return 用户信息
-     */
-    @GetMapping("/{id}")
-    public User getUser(@PathVariable Long id) {
-        // url中的id可通过@PathVariable绑定到函数的参数中
-        return users.get(id);
+    @PostMapping(value = "/getUserByName")
+    @ApiOperation(value = "根据用户名获取用户信息", notes = "根据用户名从数据库中获取用户信息")
+    public String getUserByName(@RequestBody String username) {
+        return userService.selectByUsername(username).toString();
     }
-
-    /**
-     * 处理"/users/{id}"的PUT请求，用来更新User信息
-     *
-     * @param id id
-     * @param user 用户
-     * @return 结果
-     */
-    @PutMapping("/{id}")
-    public String putUser(@PathVariable Long id, @RequestBody User user) {
-        User u = users.get(id);
-        u.setName(user.getName());
-        u.setAge(user.getAge());
-        users.put(id, u);
-        return "success";
-    }
-
-    /**
-     * 处理"/users/{id}"的DELETE请求，用来删除User
-     *
-     * @param id id
-     * @return 结果
-     */
-    @DeleteMapping("/{id}")
-    public String deleteUser(@PathVariable Long id) {
-        users.remove(id);
-        return "success";
-    }
-
 }
